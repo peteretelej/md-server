1c471768668706f6ce7e9613b052fc81a5530d00
Peter Etelej <peter@etelej.com>
2025-08-09 02:28:36 +0300

    Cleanup old tests infra

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from pathlib import Path
+
+
+@pytest.fixture
+def test_data_dir() -> Path:
+    """Return path to test data directory."""
+    return Path(__file__).parent / "test_data"
